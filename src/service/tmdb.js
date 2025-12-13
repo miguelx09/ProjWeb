41f1957336840da2d97be86fd80395416c6273e9
Miguelx09 <miguelcortes0909@gmail.com>
2025-12-13 22:35:21 +0000

    txt

--- src/service/tmdb.js
+++ src/service/tmdb.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const tmdb = tmdbv3.init('961d3f7e26beb8b8e0f9b41a6bfa6f42');
+const tmdb = tmdbv3.init('process.env.961d3f7e26beb8b8e0f9b41a6bfa6f42');
 
 // pesquisar filmes por texto
 export function searchMovies(query, callback) {
