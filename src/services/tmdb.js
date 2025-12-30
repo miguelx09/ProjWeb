858f426caba82046d2463af0dec491fd0ea7f915
Miguelx09 <miguelcortes0909@gmail.com>
2025-12-30 05:02:14 +0000

    now

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const tmdb = tmdbv3.init(process.env.TMDB_API_KEY);
+const API_KEY = process.env.TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 // pesquisar filmes por texto
 export function searchMovies(query, callback) {
@@ -15,12 +17,28 @@ export function getMovieDetails(tmdbId, callback) {
   tmdb.movie.info(tmdbId, callback);
 }
 
-// filmes populares
-export function getPopularMovies(callback) {
-  tmdb.movie.popular(callback);        // se der erro, trocar para tmdb.movie.popular_list
+// filmes populares (usa fetch direto à API TMDB)
+export async function getPopularMovies(callback) {
+  try {
+    const res = await fetch(
+      `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-PT&page=1`
+    );
+    const data = await res.json();
+    callback(null, data); // { page, results, ... }
+  } catch (err) {
+    callback(err);
+  }
 }
 
 // filmes melhor avaliados
-export function getTopRatedMovies(callback) {
-  tmdb.movie.top_rated(callback);      // este é quase sempre top_rated na tmdbv3
+export async function getTopRatedMovies(callback) {
+  try {
+    const res = await fetch(
+      `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-PT&page=1`
+    );
+    const data = await res.json();
+    callback(null, data);
+  } catch (err) {
+    callback(err);
+  }
 }
