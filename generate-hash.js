24d020c0d64dd4ab465b55a97f54be9f1a1f9b13
Miguelx09 <miguelcortes0909@gmail.com>
2026-01-09 20:29:37 +0000

    generate

---
+++ generate-hash.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+
+const password = '123456';
+
+bcrypt.hash(password, 10).then(hash => {
+  console.log('\n===========================================');
+  console.log('Hash gerado para a password "123456":');
+  console.log('===========================================');
+  console.log(hash);
+  console.log('===========================================\n');
+  console.log('Copia o hash acima e executa na base de dados:');
+  console.log(`UPDATE users SET password_hash = '${hash}';`);
+  console.log('\n');
+});
